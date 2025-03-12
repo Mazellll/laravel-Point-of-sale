8aa37bcd9c77fb898350af96863657888841a62e
Mazel <mzlaryaputra@gmail.com>
2025-03-12 13:48:18 +0700

    Table migration

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\User;
+
+class UserController extends Controller
+{
+    // public function index()
+    // {
+    //     $User = User::all(); //Menampilkan Data
+    //     // return $User;
+    //     return view ('muser', compact('User'));
+    // }
+}
--- database/migrations/2025_03_12_055439_create_items_table.php
+++ database/migrations/2025_03_12_055439_create_items_table.php
@@ -11,11 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('items', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->integer('price');
+            $table->integer('stock');
         });
     }
 
@@ -24,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('items');
     }
 };
