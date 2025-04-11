7b03409af107f903bb3430a6c0306f6282261f23
Mazel <mzlaryaputra@gmail.com>
2025-04-11 11:19:58 +0700

    Template Integration

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\TransactionDetailController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +25,11 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::resource('/category', CategoryController::class);
+Route::resource('/item', ItemController::class);
+Route::resource('/transaction', TransactionController::class);
+Route::resource('/transdetail', TransactionDetailController::class);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
